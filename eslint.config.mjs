7a71e3e0ae4458754bc08a688bbf5379748f915e
jonghwann <jonghwan0729@gmail.com>
2025-07-23 20:53:04 +0900

    feat: supabase DB 연결

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import eslint from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
-import importPlugin from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
