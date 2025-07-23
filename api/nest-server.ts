9f0d85498fe5b981e148e1668fefe2555dc50e11
jonghwann <jonghwan0729@gmail.com>
2025-07-23 23:19:46 +0900

    test: Swagger 적용 테스트

--- api/nest-server.ts
+++ api/nest-server.ts
@@ -1,6 +1,7 @@
 import { IncomingMessage, ServerResponse } from 'http';
 
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Express } from 'express';
 
 import { AppModule } from '../src/app.module';
@@ -10,6 +11,15 @@ let cachedHandler: Express | null = null;
 export default async function handler(req: IncomingMessage, res: ServerResponse) {
   if (!cachedHandler) {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+      .setTitle('API 문서')
+      .setDescription('Nest.js 서버리스 Swagger 문서입니다.')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+
     await app.init();
     cachedHandler = app.getHttpAdapter().getInstance() as Express;
   }
