20e2bad597c822ecf7e561c1b1d3ef85144eb5d4
jangjonghwan <wkdwhdghks97@gmail.com>
2025-07-23 23:19:46 +0900

    test: Swagger 적용 테스트

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,8 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
-    }),
-  ],
-  providers: [AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API 문서')
+    .setDescription('Nest.js Swagger 문서입니다.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
