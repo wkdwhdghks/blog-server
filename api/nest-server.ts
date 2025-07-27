e49beaf38ad3970af4f11d95ec5e63af6a7a0ae5
jangjonghwan <wkdwhdghks97@gmail.com>
2025-07-27 22:33:01 +0900

    chore: Swagger UI CDN 적용

--- api/nest-server.ts
+++ api/nest-server.ts
@@ -3,8 +3,7 @@ import { IncomingMessage, ServerResponse } from 'http';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Express } from 'express';
-
-import { AppModule } from '../src/app.module';
+import { AppModule } from 'src/app.module';
 
 let cachedHandler: Express | null = null;
 
@@ -17,8 +16,16 @@ export default async function handler(req: IncomingMessage, res: ServerResponse)
       .setDescription('Nest.js 서버리스 Swagger 문서입니다.')
       .setVersion('1.0')
       .build();
+
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('swagger', app, document);
+
+    SwaggerModule.setup('swagger', app, document, {
+      customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      customJs: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+      ],
+    });
 
     await app.init();
     cachedHandler = app.getHttpAdapter().getInstance() as Express;
