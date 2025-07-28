9c0079b2b7abdea3a4a3c71253af50d7b9aa48cf
jangjonghwan <wkdwhdghks97@gmail.com>
2025-07-28 21:47:38 +0900

    feat: PrismaService 생성 및 전역 모듈 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [PrismaModule],
 })
 export class AppModule {}
