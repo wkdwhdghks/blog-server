371a6865c1cd02c34a3de63862a497627e15568c
jonghwann <jonghwan0729@gmail.com>
2025-09-26 15:43:47 +0900

    feat: Supabase Storage를 사용한 이미지 업로드 API 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { PostsModule } from './posts/posts.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { SearchModule } from './search/search.module';
 import { TagsModule } from './tags/tags.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [AuthModule, PostsModule, PrismaModule, SearchModule, TagsModule],
+  imports: [AuthModule, PostsModule, PrismaModule, SearchModule, TagsModule, UploadModule],
 })
 export class AppModule {}
